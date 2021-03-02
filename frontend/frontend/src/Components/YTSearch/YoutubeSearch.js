96bcf25e2794107e249f586f3d996ddad0b64a06
“Jawnny5” <digitalanalogbros@gmail.com>
2021-03-02 14:32:32 -0700

    ytsearch function

--- frontend/frontend/src/Components/YTSearch/YoutubeSearch.js
+++ frontend/frontend/src/Components/YTSearch/YoutubeSearch.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import YTSearch from 'youtube-api-search'
 export default function YoutubeSearch(props) {
+  const videoSearch = () => {
+    YTSearch({
+      key:"AIzaSyB9Ytuql9tpCpmFtHjM5ZnPFwVdm3nMe7c" , 
+      term: props.searchTerm }, (videos) => {
+        console.log(videos)
+    })
+  }
   return (
     <div>
-      
+      {videoSearch}
     </div>
   )
 }
